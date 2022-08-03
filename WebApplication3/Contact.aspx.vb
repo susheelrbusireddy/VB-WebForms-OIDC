6c69e904a27cd44d3d281eaccfdf6a67840983e3
susheelrbusireddy <58010360+susheelrbusireddy@users.noreply.github.com>
2022-08-03 09:59:35 -0500

    Protected Contact page with OIDC

--- WebApplication3/Contact.aspx.vb
+++ WebApplication3/Contact.aspx.vb
@@ -1,7 +1,14 @@
-﻿Public Class Contact
+﻿Imports Microsoft.AspNet.Identity
+Imports Microsoft.Owin.Security
+Imports Microsoft.Owin.Security.OpenIdConnect
+Public Class Contact
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
+        If Not (Request.IsAuthenticated) Then
+            HttpContext.Current.GetOwinContext().Authentication.Challenge(New AuthenticationProperties() With {
+                                                              .RedirectUri = "/"},
+                                                              OpenIdConnectAuthenticationDefaults.AuthenticationType)
+        End If
     End Sub
 End Class
